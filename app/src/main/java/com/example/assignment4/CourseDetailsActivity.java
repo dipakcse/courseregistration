b52d272490377c509a445c83a451d943f42bb679
Dipak Kumar Mondal <dipak.cse2004@gmail.com>
2021-04-13 19:32:40 -0300

    code

--- app/src/main/java/com/example/assignment4/CourseDetailsActivity.java
+++ app/src/main/java/com/example/assignment4/CourseDetailsActivity.java
@@ -26,6 +26,7 @@ public class CourseDetailsActivity extends AppCompatActivity {
             prerequsite.setText(dataModel.getPrerequsite());
         }catch (Exception e){
             e.printStackTrace();
+
         }
     }
 }
